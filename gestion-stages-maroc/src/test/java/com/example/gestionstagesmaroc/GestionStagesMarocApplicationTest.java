4e7051daa90c99b043572f939a37ba09789b196c
aziza-halmaouy <a.halmaouy7871@uca.ac.ma>
2026-05-22 16:53:00 +0100

    Fix CI tests config

---
+++ gestion-stages-maroc/src/test/java/com/example/gestionstagesmaroc/GestionStagesMarocApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.gestionstagesmaroc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionStagesMarocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
